e95c999947b5a99a1eb72eab0a9202bd3772d5e3
Saumer7 <31887385+Saumer7@users.noreply.github.com>
2021-09-23 19:15:14 +0900

    Update reboot.c

--- kernel/reboot.c
+++ kernel/reboot.c
@@ -16,7 +16,7 @@
 #include <linux/syscalls.h>
 #include <linux/syscore_ops.h>
 #include <linux/uaccess.h>
-#include <linux/sec_debug.h>
+/*#include <linux/sec_debug.h>*/
 
 /*
  * this indicates whether you can reboot with ctrl-alt-del: the default is yes
